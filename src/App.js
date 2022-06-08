46a0cfed8bd3ff8677d357e866fd8076c7882394
Tanofxp <danifxp@gmail.com>
2022-06-08 00:38:43 -0300

    navBar o menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.png';
-import './App.css';
 
-function App() {
+import './App.css';
+import Menu from "./components/menu";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Hola Soy una app de React !
-        </p>
-        <a
-          className="App-link"
-          href="https://dirtec.com.ar"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vea mas Sobre Dirtec
-        </a>
-      </header>
-    </div>
+  <div class="text-center menu">
+    <div> <Menu /></div> 
+  </div>
   );
 }
 
