7c979e96751c0c038c97e0adbf15552e8b3e1341
Tanofxp <danifxp@gmail.com>
2022-06-15 22:12:42 -0300

    mobiendo boton agregar al carrito

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import ItemListContainer from './components/ItemListContainer';
 import NavBar from "./components/NavBar";
 import logo from './pngwing.com.png';
 
-function agregar(){
-  alert("Su producto se agrego correctamente")
-};
 
 function App() {
   let maxStock = 10;
@@ -15,13 +12,10 @@ function App() {
   <div className="text-center menu">
     <div> <NavBar /></div>
     <div className='d-flex justify-content-center'>
-    <div className='card  text-dark m-2 p-1 border border-5 border-info bg-warning' >
-        <img src={logo} className="" alt="producto" />
+    <div className='card  text-dark m-1  border border-3 border-info bg-warning' >
+        <img src={logo} alt="producto" />
         <ItemListContainer  miProd={ miProd } maxStock={maxStock}/>
     </div> 
-    </div>
-    <div>
-          <button onClick={()=>{agregar()}} className='btn btn-primary mb-2'>Agregar al Carrito</button>
     </div>
   </div>
   );
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,6 +5,9 @@ export default function ItemListContainer({miProd, maxStock}) {
 
   let [auxStock, setAuxStock] = useState(maxStock);
   
+  function agregar(){
+    alert("Su producto se agrego correctamente")
+  };
   
   function aumentarStock(){
 
@@ -17,7 +20,7 @@ export default function ItemListContainer({miProd, maxStock}) {
   }
 
   return (
-    <div className='text-white h1 text-capitalize mt-5'>
+    <div className='text-white h1 text-capitalize mt-3'>
     <div className='container-fluid'>
         <h5>Producto {miProd.Marca}</h5>
         <h5 className='m-4'>Modelo {miProd.Modelo}</h5>
@@ -25,8 +28,11 @@ export default function ItemListContainer({miProd, maxStock}) {
         <div className='row'>
         <div className='col'>
         <button className='btn btn-danger mb-2' onClick={()=>{restarStock();}}>-</button>
-        <input className='col-3 m-3 text-center' value={auxStock} disabled></input>
+        <input className='col-3 m-2 text-center' value={auxStock} disabled></input>
         <button className='btn btn-success mb-2' onClick={()=>{aumentarStock();}}>+</button>
+        </div>
+        <div>
+          <button onClick={()=>{agregar()}} className='btn btn-primary mb-2'>Agregar al Carrito</button>
         </div>
         </div>
     </div>
